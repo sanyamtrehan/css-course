f5a4d8d7331e606767fa9104c8db7365254ae1d5
Sanyam Trehan <sanyam.sanyam.trehan@gmail.com>
2023-04-11 19:46:33 +0530

    sticky navigation

--- Omnifood/js/script.js
+++ Omnifood/js/script.js
@@ -44,6 +44,29 @@ allLinks.forEach((link) => {
 });
 
 ///////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  ([entry]) => {
+    const { isIntersecting } = entry;
+    const body = document.querySelector("body");
+    if (!isIntersecting) {
+      body.classList.add("sticky");
+    } else {
+      body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
